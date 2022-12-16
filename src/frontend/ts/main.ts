4d23d672b96dbdda70c2e88af2e39781d9b71d7d
Fabian Banderas <fabian.banderasb@gmail.com>
2022-12-15 23:00:53 -0500

    Add changes

--- src/frontend/ts/main.ts
+++ src/frontend/ts/main.ts
@@ -1,6 +1,44 @@
+class Main{
+    private nombre: string;
+    public edad: number;
 
-function SayHello(){
-    let current_value = document.getElementById("textarea_1") as HTMLInputElement;
-    let new_value = "Hello world!!!" + "\n" + current_value.value;
-    document.getElementById("textarea_1").innerHTML = new_value;
+    constructor(nombre:string){
+        this.nombre = nombre;
+        
+    }
+    public getNombre():string{
+        return this.nombre;
+    }
+
+    public toString():string{
+        return "Nombre: "+this.nombre+ " Edad: "+this.edad; 
+    }
+}
+
+
+
+window.onload = inicio//para que ejecute luego de cargar la página
+
+function inicio(){
+
+    let main:Main = new Main("Fabian");
+    main.edad = 29;
+    //let nombre = main.getNombre();
+    //alert("Hola "+ nombre + " tenés "+ main.edad + " años");
+    mostrar(main);
+    let main2:Main = new Main("Romyna");
+    main2.edad = 33;
+    mostrar(main2);
+
+    let btn = document.getElementById("btnSaludar");
+    
+    btn.onclick =  saludar;
+}
+
+function mostrar(main:Main){
+    alert(main.toString());
+}
+
+function saludar(){
+    alert("Hola");
 }
