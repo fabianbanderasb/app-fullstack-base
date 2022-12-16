5302aef0b0be709b4944029e04a2b9a0611fecfa
Fabian Banderas <fabian.banderasb@gmail.com>
2022-12-16 15:11:09 -0500

    Add changes

--- src/frontend/ts/main.ts
+++ src/frontend/ts/main.ts
@@ -11,10 +11,42 @@ class Main implements EventListenerObject{
         return this.personas;
     }
 
+    public consultarDispositivosAlServidor(){ //capturar los dispositivos del servidor
+        let xmlHttp = new XMLHttpRequest();
+        xmlHttp.onreadystatechange = () =>{ //se va ejecutar cuando el servidor tenga la información disponible para mostrar
+            if(xmlHttp.readyState==4){
+                console.log("Llegó info del servidor", xmlHttp.responseText);    
+            }
+            
+        }
+        xmlHttp.open("GET", "http://localhost:8000/devices",true);
+        console.log("Abrir la conexión");
+        xmlHttp.send();
+        console.log("Envie la consulta");
+    }
+
     handleEvent(object: Event): void {
 
-        console.log(object);
-        alert(`Hola ${this.personas[0].getNombre()} estoy en el main`);
+        //console.log(object.type);
+        //console.log(object.target);
+        let tipoEvento: string = object.type;
+        let objEvento: HTMLElement;
+        objEvento = <HTMLElement>object.target;
+        if(objEvento.id == "btnOtro"){
+            console.log(objEvento.id, objEvento.textContent);
+
+            let iNombre = <HTMLInputElement>document.getElementById("iNombre");
+            //objEvento.textContent = "Cambiar el nombre";
+            objEvento.textContent = iNombre.value;
+            alert(`Hola ${this.personas[0].getNombre()} estoy en el main`);
+        }else if(objEvento.id == "btnSaludar"){
+            let textArea = document.getElementById("textarea_1");
+            //textArea.textContent = `Hola ${this.personas[1].getNombre()} otro botón`;
+            textArea.innerHTML = `Hola ${this.personas[1].getNombre()} otro botón`;
+            //alert(`Hola ${this.personas[1].getNombre()} otro botón`);
+
+            this.consultarDispositivosAlServidor();//llamado a la función consultarDispositivosAlServidor
+        }
     }
 }
 
@@ -26,8 +58,9 @@ window.addEventListener("load",()=>{
     main.addPersona(new Persona("Romyna"));
     mostrar(main);
     let btn = document.getElementById("btnSaludar");
-    let btn2 = document.getElementById("btnOtro");
     btn.addEventListener("click", main);
+    let btn2 = document.getElementById("btnOtro");
+    btn2.addEventListener("click", main);
 
     //btn.onclick = function(){
     //    alert("Hola");
@@ -53,5 +86,5 @@ function mostrar(main:Main){
         datosPersonas = datosPersonas + personas[i].toString();
     }
 
-    alert(datosPersonas);
+    //alert(datosPersonas);
 }
